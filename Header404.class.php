dd3f47a8b5cd8b0546c02bd13399f3f5a762254d
scopika <cyril@scopika.com>
2013-04-03 13:55:44 +0200

    v1.1

---
+++ Header404.class.php
@@ -0,0 +1,39 @@
+<?php
+include_once(realpath(dirname(__FILE__)) . "/../../../classes/PluginsClassiques.class.php");
+
+class Header404 extends PluginsClassiques{
+    
+    /**
+     * Adaptation du code du fichier fonctions/moteur.php pour gérer les pages 404
+     * En cas d'erreur, on laisse la main au moteur Thelia
+     */ 
+	function pre() {
+		global $res, $reptpl, $fond;
+
+		// chargement du squelette
+		if($res == "") {
+
+            $tpl = $reptpl . $fond;
+
+            // $tpl doit impérativement être dans le répertoire $reptpl, ou un de ses sous répertoires.
+            $path_tpl = rtrim(dirname($tpl), '/');
+            $path_reptpl = rtrim($reptpl, '/');
+            if (strpos($path_tpl, $path_reptpl) !== 0)  return; // on laisse la main au moteur Thelia
+
+            if (!file_exists($tpl)) {
+                $tpl .= ".html";
+                if(!file_exists($tpl)) return; // on laisse la main au moteur Thelia
+            }
+			
+			// page non trouvée => page 404
+            header("HTTP/1.1 404 Not Found");
+            // On cherche un fichier 404.html dans le dossier $reptpl, sinon on prend celui fourni avec le plugin
+            foreach(array($reptpl . '404.html', 'client/plugins/header404/404.html') as $template) {
+                if(file_exists($template)) {
+                    $res = file_get_contents($template);
+                    break;
+                }
+            }
+		}
+	}	
+}
